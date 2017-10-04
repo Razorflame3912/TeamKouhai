e22997c9c667f03476fe476485b332b567d4f332
Mansour <melsharawy@stuy.edu>
2017-10-04 14:47:50 -0400

    classwork

---
+++ login.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, session, redirect
+
+app = Flask(__name__)
+app.secret_key = 'THE TING GO SKRRRA PA PA KA KA KA'
+
+
+@app.route('/')
+def root():
+    if 'user' in session:
+        return redirect('/welcome')
+    return render_template('login.html')
+
+@app.route('/welcome', methods=['GET','POST'])
+def welcome():
+
+    if 'user' not in session:
+        if 'username' not in request.form:
+            return 'you stupid: go to root'
+        elif request.form['username'] != u'':
+            enteredUser = request.form['username']
+            print repr(enteredUser)
+        else:
+            return 'you stupid: no user'
+    
+        if 'pass' not in request.form:
+            return 'you stupid: go to root'
+        elif request.form['pass'] != u'':
+            enteredPass = request.form['pass']
+            print enteredPass
+        else:
+            return 'you stupid: no pass'
+        session['user'] = enteredUser
+        session['pass'] = enteredPass
+        return 'hi! First time!'
+    else:
+        return 'Hi ' + session['user'] + '!'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
